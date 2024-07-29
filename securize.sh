5a0a4c992439e0f7e6f8e663b47daa6ceb88ee77
Michael Peeters <52107159+Artzert2215@users.noreply.github.com>
2024-07-29 13:49:25 +0200

    Remove files for vndklite or A-only devices
    This is a personal project, by device is arm64 ab (non vndklite).

--- securize.sh
+++ securize.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 #Usage:
-#bash lite-adapter.sh <32|64> [/path/to/system/image]
+#sudo bash securize.sh [/path/to/system/image]
 
 #cleanups
 umount d
-
 set -ex
-
 origin="$(readlink -f -- "$0")"
 origin="$(dirname "$origin")"
 
@@ -16,18 +14,18 @@ if [ -f "$1" ];then
 fi
 
 if [ ! -f "$srcFile" ];then
-	echo "Usage: sudo bash securize.sh <32|64> [/path/to/system.img]"
+	echo "Usage: sudo bash securize.sh [/path/to/system/image]"
 	exit 1
 fi
 
-"$origin"/simg2img "$srcFile" s-secure.img || cp "$srcFile" s-secure.img
+"$origin"/simg2img "$srcFile" slimmed.img || cp "$srcFile" slimmed.img
 
 rm -Rf tmp
 mkdir -p d tmp
-e2fsck -y -f s-secure.img
-resize2fs s-secure.img 3500M
-e2fsck -E unshare_blocks -y -f s-secure.img
-mount -o loop,rw s-secure.img d
+e2fsck -y -f slimmed.img
+resize2fs slimmed.img 3500M
+e2fsck -E unshare_blocks -y -f slimmed.img
+mount -o loop,rw slimmed.img d
 
 touch d/system/phh/secure
 rm d/system/xbin/su
@@ -40,5 +38,5 @@ sleep 1
 
 umount d
 
-e2fsck -f -y s-secure.img || true
-resize2fs -M s-secure.img
+e2fsck -f -y slimmed.img || true
+resize2fs -M slimmed.img
